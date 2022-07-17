263a0b6114adc5187f60197f38874354b5e7ea44
Christopher DiStasio <cadistasio@gmail.com>
2022-07-17 13:10:14 -0500

    Sets up, completes controller, ex obj

---
+++ controllers/breads_controller.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const Bread = require('../models/bread.js');
+
+
+// INDEX
+router.get('/', (req, res) => {
+  res.send(Bread);
+})
+
+
+
+module.exports = router
+
+// SHOW
+router.get('/:arrayIndex', (req, res) => {
+  res.send(Bread[req.params.arrayIndex])
+})
